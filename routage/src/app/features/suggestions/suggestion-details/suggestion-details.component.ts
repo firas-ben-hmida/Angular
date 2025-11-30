6e365299cc963120d0f7e5c340e265b8c1e1991b
firas <firas.benhmida21@gmail.com>
2025-11-30 23:18:30 +0100

    routage

---
+++ routage/src/app/features/suggestions/suggestion-details/suggestion-details.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Suggestion } from '../../../models/suggestion';
+import { SuggestionService } from '../../../core/services/suggestion.service';
+
+@Component({
+  selector: 'app-suggestion-details',
+  templateUrl: './suggestion-details.component.html',
+  styleUrls: ['./suggestion-details.component.css']
+})
+export class SuggestionDetailsComponent implements OnInit {
+  suggestion!: Suggestion | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public suggestionService: SuggestionService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = +params['id'];
+      this.suggestion = this.suggestionService.getById(id);
+    });
+  }
+
+  incrementLikes(): void {
+    if (this.suggestion) {
+      this.suggestionService.incrementLikes(this.suggestion.id);
+    }
+  }
+
+  addToFavorites(): void {
+    if (this.suggestion) {
+      if (!this.suggestionService.isFavorite(this.suggestion.id)) {
+        this.suggestionService.addToFavorites(this.suggestion.id);
+        alert('Suggestion ajoutée aux favoris: ' + this.suggestion.title);
+      } else {
+        alert('Cette suggestion est déjà dans les favoris');
+      }
+    }
+  }
+
+  isFavorite(): boolean {
+    return this.suggestion ? this.suggestionService.isFavorite(this.suggestion.id) : false;
+  }
+
+  goBack(): void {
+    this.router.navigate(['/suggestions']);
+  }
+}
